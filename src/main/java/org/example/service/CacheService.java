0ddc6aa240d0c00ae68fb6d8f619cd5fe4a8de4e
sumedhrajput <rajputsumedhsingh@gmail.com>
2025-11-19 20:01:10 +0530

    first commit

---
+++ src/main/java/org/example/service/CacheService.java
@@ -0,0 +1,10 @@
+package com.example.bms.service;
+
+public interface CacheService {
+
+     void set(String key,Object value);
+     Object get(String key);
+     void delete(String key);
+     void getAllKeysAndValues();
+     void deleteAll();
+}
