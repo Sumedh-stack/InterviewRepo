cafd01ed2138396b78901e8145fda898e3115ad2
sumedhrajput <rajputsumedhsingh@gmail.com>
2025-11-19 20:19:53 +0530

    add model

---
+++ src/main/java/org/example/model/BaseModel.java
@@ -0,0 +1,33 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+}
---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,25 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User extends BaseModel{
+
+   private String name;
+   private String email;
+
+//   @ManyToOne
+//   private Attribute attribute1;
+//
+//   @OneToMany(mappedBy = "otherAttributeName")
+//   List<Attribute> attribute2;
+//
+//   @Enumerated(EnumType.ORDINAL)
+//   private Attribute attribute3;
+}
